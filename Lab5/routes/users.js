8987d1a308c7b89cfe5358d683fe5cfe57abcf31
FikruMengesha <fikrumengesha@gmail.com>
2017-07-01 09:01:46 -0500

    using promise

--- Lab5/routes/users.js
+++ Lab5/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var fetch = require('node-fetch');
 var data = {ths: 'data'}
 // Get All data
 router.get('/users', function(req, res, next){
@@ -13,3 +13,11 @@ router.get('/users', function(req, res, next){
         res.send('promise');
     });
 
+// fetch('http://jsonplaceholder.typicode.com/users/')
+//         .then(function (res) {
+//                 return res.text();
+//         }).then(function (body) {
+//                 console.log(body);
+//         }).catch(console.log('rejected'));
+
+module.exports =router;
