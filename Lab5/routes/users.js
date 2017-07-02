ea25cf1522aff9e0c04e1502d42deadf48a104e2
FikruMengesha <fikrumengesha@gmail.com>
2017-07-01 21:10:43 -0500

    progress

--- Lab5/routes/users.js
+++ Lab5/routes/users.js
@@ -1,23 +1,45 @@
+const rx = require('@reactivex/rxjs');
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
-var data = {ths: 'data'}
-// Get All data
-router.get('/users', function(req, res, next){
-    
-           
-              res.json(data);
-    });
+var promise = fetch('http://jsonplaceholder.typicode.com/users/');
 
-    router.get('/promise', function(req,res,next){
-        res.send('promise');
-    });
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
 
-// fetch('http://jsonplaceholder.typicode.com/users/')
-//         .then(function (res) {
-//                 return res.text();
-//         }).then(function (body) {
-//                 console.log(body);
-//         }).catch(console.log('rejected'));
+router.get('/promise', function (req, res, next) {
 
-module.exports =router;
+  promise.then(data => data.json())
+    //.then(data => res.send(data))
+    .then(data=>{res.render('users',{users: data})})
+    .catch((err) => { console.log(err) });
+});
+
+router.get('/observable', function (req, res, next) {
+  var response = rx.Observable.fromPromise(promise);
+  response.map((data) => data.json()
+    .subscribe((data) => res.send(data)));
+  //res.send('observabele');
+});
+
+router.get('/asyncawait', function (req, res, next) {
+asyncAwait(req);
+//   res.send('from syncawait');
+});
+
+async function asyncAwait(req) {
+  try {
+    let results = await promise;
+    result.then(data => data.json())
+      .then(data => res.send(data))
+      .catch((err) => { console.log(err) });
+  } catch (err) {
+    console.log(Error.message);
+
+  }
+
+}
+
+module.exports = router;
